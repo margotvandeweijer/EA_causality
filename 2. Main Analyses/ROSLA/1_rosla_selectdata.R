83b777194c325559522229c754e811dc02edaadd
Margot van de Weijer <109525818+margotvandeweijer@users.noreply.github.com>
2022-07-18 15:36:39 +0200

    Update and rename 1_rosla_selectdata (2).R to 1_rosla_selectdata.R

--- 2. Main Analyses/ROSLA/1_rosla_selectdata.R
+++ 2. Main Analyses/ROSLA/1_rosla_selectdata.R
@@ -1,19 +1,16 @@
 #ROSLA analyses
 # data prep
-complete = read.csv("/home/margotw/EA_WB/data/full_data/complete.csv", header=T) #full dataset without related individuals
-
+complete = read.csv("complete.csv", header=T) #full standardized dataset without related individuals
 complete = subset(complete, !is.na(education))
 
-#we first need to create a variable that indicates if someone was affected by the reform or not
+#we first create a variable indicating if someone was affected by the reform or not
 
 #month of birth
-#mob = read.csv("/home/margotw/EA_WB/data/covariates/mob.out.csv")
+#mob = read.csv("mob.out.csv")
 #colnames(mob)[2] <- "mob"
-
 #complete = merge(complete, mob, by="eid")
 
 #anyone born after 1 September 1957 was affected by the reform
-
 complete$reform <- 0
 complete$reform[complete$yob > 1957] <- 1
 complete$reform[complete$yob == 1957 & complete$mob > 8] <- 1
@@ -27,5 +24,4 @@ complete$delete[complete$yob == 1955 & complete$mob <3] <- 1
 complete$delete[complete$yob == 1960 & complete$mob >2] <- 1
 complete = subset(complete, delete == 0)
 
-
-write.csv(complete,"/home/margotw/EA_WB/data/full_data/rosla.csv", quote=F, row.names=F)
+write.csv(complete,"rosla.csv", quote=F, row.names=F)
